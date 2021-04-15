5d8995c2eb71b406534dd4eb82e66212975cafb6
Jordan <jordan.ramos@columbia.edu>
2021-04-15 15:05:20 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from twilio.twiml.messaging_response import MessagingResponse
 from davidbot import ask, append_interaction_to_chat_log
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '6164985632-test2'
+app.config['SECRET_KEY'] = '6164985632-test4'
 
 @app.route('/david', methods=['POST'])
 def david():
